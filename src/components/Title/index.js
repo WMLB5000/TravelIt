d343e6693b8eb9630d542113cf4c5cc19799271b
WMLB5000 <leroux172@gmail.com>
2023-01-27 11:21:52 +0200

    Title Change

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,7 +3,7 @@ import {Text} from 'react-native';
 import styles from './styles';
 
 const Title = () => {
-  return <Text style={styles.title}>Title Component</Text>;
+  return <Text style={styles.title}>Title Component Hello</Text>;
 };
 
 export default React.memo(Title);
